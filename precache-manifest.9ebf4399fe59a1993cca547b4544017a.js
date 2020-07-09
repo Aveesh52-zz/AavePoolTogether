ffde706590f3a32b5fb52f8d747538835c8d01a3
Aveesh52 <aveeshshetty007@gmail.com>
2020-07-09 20:32:03 +0530

    Updates

---
+++ precache-manifest.9ebf4399fe59a1993cca547b4544017a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc1b57c38088dbdf459a",
+    "url": "/static/css/main.b96c94dc.chunk.css"
+  },
+  {
+    "revision": "fc1b57c38088dbdf459a",
+    "url": "/static/js/main.fc1b57c3.chunk.js"
+  },
+  {
+    "revision": "b5b3f98ea673888338e9",
+    "url": "/static/css/1.673458d6.chunk.css"
+  },
+  {
+    "revision": "b5b3f98ea673888338e9",
+    "url": "/static/js/1.b5b3f98e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63cf8f79a22d6d072c80d0aeda8b42d9",
+    "url": "/static/media/Dai.63cf8f79.png"
+  },
+  {
+    "revision": "584253aca7c56f0db6c481ed1453e282",
+    "url": "/static/media/sitting-2@2x.584253ac.png"
+  },
+  {
+    "revision": "5be673d6eff901a1b89c7f2d10541a53",
+    "url": "/static/media/standing-9.5be673d6.png"
+  },
+  {
+    "revision": "732843706ad6697421582c6664f74a70",
+    "url": "/index.html"
+  }
+];
